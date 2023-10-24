e3e4890dc938ca308b7f398f048ec713ddb9b871
Radek <rajkonkret660@gmail.com>
2023-10-24 15:27:34 +0200

    2 - add petle3

---
+++ petla_zad3.py
@@ -0,0 +1,25 @@
+# while - petle sterowana warunkiem
+
+licznik = 0
+while True:
+    licznik += 1
+    print("komunikat1")
+    if licznik > 10:
+        break  # przerwanie bieacej pętli
+print(licznik)  # 11
+licznik = 0
+while licznik < 10:
+    print("Komunikat2")
+    licznik += 1
+
+lista = []
+lista2 =[]
+while True:
+    wej = input("Podaj liczbę")
+    if not wej.isnumeric():
+        break
+    lista.append(wej)
+    lista2.append(int(wej))
+
+print(lista)  # ['4', '5', '6', '7'] lista stringow
+print(lista2)  # [4, 5, 6, 7]  - int
