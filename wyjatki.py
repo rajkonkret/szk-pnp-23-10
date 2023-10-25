210cf17337c806c2a404e6d13e1a968c58caa8d5
Radek <rajkonkret660@gmail.com>
2023-10-25 09:25:20 +0200

    3 - update wyjatki

--- wyjatki.py
+++ wyjatki.py
@@ -15,19 +15,31 @@ while True:
     odp = input("Podaj opcje z menu")
     if odp >= "5":
         break
-
-    a = float(input("Podaj pierwszą liczbę"))
-    b = float(input("Podaj drugą liczbę"))
-    if odp == '1':
-        print(a + b)
-    elif odp == '2':
-        print(a - b)
-    elif odp == '3':
-        print(a * b)
-    elif odp == '4':
-        if b != 0:
+    try:
+        a = float(input("Podaj pierwszą liczbę"))
+        b = float(input("Podaj drugą liczbę"))
+        if odp == '1':
+            print(a + b)
+        elif odp == '2':
+            print(a - b)
+        elif odp == '3':
+            print(a * b)
+        elif odp == '4':
+            # if b != 0:
             print(a / b)
+            # else:
+            # print("Nie dziel przez zero")
         else:
-            print("Nie dziel przez zero")
+            print("nie znam takiego działania")
+    except ZeroDivisionError:
+        print("Nie dziel przez zero")
+    except ValueError:
+        print("Błąd wartości")
+    except TypeError:
+        print("Błąd typu")
+    except Exception as e:
+        print("Wystąpił błąd", e)
     else:
-        print("nie znam takiego działania")
+        print("Tylko gdy nie ma błedu")
+    finally:
+        print("Wykonuje się zawsze")
